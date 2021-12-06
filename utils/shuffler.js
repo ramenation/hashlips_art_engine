d4e3116ea7476b303733cb6e93ba681471d2a281
ramenation <93734735+ramenation@users.noreply.github.com>
2021-12-07 02:10:02 +0800

    Reworked shuffle
    get all filenames, rename into a shuffled sequential order

--- utils/shuffler.js
+++ utils/shuffler.js
@@ -1,4 +1,5 @@
 const basePath = process.cwd();
+const { SSL_OP_EPHEMERAL_RSA } = require("constants");
 const fs = require("fs");
 
 let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
@@ -7,16 +8,39 @@ let editionSize = data.length;
 
 
 //create array of numbers from edition size
-const dataLength = Array.from({length: editionSize}, (x, i) => i);
-const shuffleLength = Array.from({length: editionSize}, (x, i) => i);
+dataLength = Array.from({length: editionSize}, (x, i) => i);
+shuffleLength = Array.from({length: editionSize}, (x, i) => i);
 
+//run shuffle
+shuffleLength = shuffle(shuffleLength);
 
-shuffle(shuffleLength);
-dataLength.forEach(printThis);
+//get filenames
+fileNames = getFilenames();
+
+//rename files according to shuffle with .temp extension to avoid conflicts
+renameFile(fileNames,shuffleLength);
+
+//remove .temp from files
 dataLength.forEach(removeTemp);
 
+//add metadeta reminder
+
+function renameFile(fileArray,shuffleArray,__callback){
+for (let i = 0; i < fileArray.length; i++) {
+    fs.rename(`${basePath}/build/images/${fileArray[i]}.png`, `${basePath}/build/images/${shuffleArray[i]}.png.temp`, function(err) {
+        console.log(`Updated ${fileNames[i]}.png to ===> ${shuffleLength[i]}.png.temp`);
+        if ( err ) console.log('ERROR: ' + err);
+    });
+    fs.rename(`${basePath}/build/json/${fileArray[i]}.json`, `${basePath}/build/json/${fileArray[i]}.json.temp`, function(err) {
+        console.log(`Updated  ${fileNames[i]}.json to ===> ${shuffleLength[i]}.json.temp`);
+        if ( err ) console.log('ERROR: ' + err);
+    });
+  }
+__callback;
+}
+
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-// randomizing sort
+// randomizing sort via Knuth shuffle - interesting reading about randomness
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
   
@@ -35,54 +59,41 @@ function shuffle(array) {
     return array;
   }
 
-
-/*
-// Load the filesnames into an array
-const directoryPath = path.join(__dirname, 'Documents');
-cons dirPath = (`${basePath}/build/images/`);
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
+// Load all existing filenames into an array
+function getFilenames(){
+    const dirPath = (`${basePath}/build/images/`);
+    var filenumber=[];
+    var numFile=[];
+    var counter = 0;
+    var thisFile = fs.readdirSync(dirPath, function (err, files) {
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file); 
     });
-});
-*/
+    numFile = thisFile.forEach(function (file) {
+        // strip out extension
+        filenumber[counter] = file.substr(0, file.indexOf('.'));
+        counter++;
+      });
 
-// rename pass 1 
-// https://www.codegrepper.com/code-examples/javascript/get+an+array+of+all+files+in+directory+using+node+js 
-// consider renaming based on filename array
-function printThis(item){
-    //console.log(item + " , " + shuffleLength[item]);
+    
+    return filenumber;
+  
+};
 
-    fs.rename(`${basePath}/build/images/${item}.png`, `${basePath}/build/images/${shuffleLength[item]}.png.temp`, function(err) {
-        if ( err ) console.log('ERROR: ' + err);
-    });
 
-    fs.rename(`${basePath}/build/json/${item}.json`, `${basePath}/build/json/${shuffleLength[item]}.json.temp`, function(err) {
-        if ( err ) console.log('ERROR: ' + err);
-    });
-   //update metadata pass 
-}
-
-//rename past temp
-function removeTemp(item){
-    // console.log(item + " , " + shuffleLength[item]);
-
+//remove .temp extensions
+//rework for async function to add promise
+function removeTemp(item,__callback){
     fs.rename(`${basePath}/build/images/${item}.png.temp`, `${basePath}/build/images/${item}.png`, function(err) {
+        console.log(`Updated ${item}.png.temp to ===> ${item}.png`);
         if ( err ) console.log('ERROR: ' + err);
     });
-
     fs.rename(`${basePath}/build/json/${item}.json.temp`, `${basePath}/build/json/${item}.json`, function(err) {
+        console.log(`Updated ${item}.json.temp to ===> ${item}.json`);
         if ( err ) console.log('ERROR: ' + err);
     });
-   
+   __callback;
 }
 
-
-//construct new _metadata file
